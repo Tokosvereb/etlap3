d934a399afa7e2db72bf87c0485a551cdf89d82f
Tokosvereb <gergok0314@gmail.com>
2023-10-11 13:38:48 +0200

    etlap3

--- kozos_eljarasok.py
+++ kozos_eljarasok.py
@@ -1,16 +1,7 @@
 def jelekkelkiiras(jel, db):
     print(jel * db)
 
-def levesek(leves1,leves2):
-    print(leves1, leves2)
-
-def foetelek(foetel1,foetel2 ):
-    print(foetel1, foetel2)
-
-def italok(ital1,ital2 ):
-    print(ital1, ital2)
-
-def desszertek(desszert1, desszert2):
-    print(desszert1, desszert2)
-
+def etelkiiras(szoveg,db):
+    # print("*    ",szoveg,"   *")
+    print(f"*{szoveg:^{db-2}}*")
 
